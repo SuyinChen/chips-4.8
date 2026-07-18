83e10dfbf1df0c25f5d06c761aa9cbcccfa099c4
Weizhen Jiang <weizhen_jiang@berkeley.edu>
2026-07-18 05:02:44 +0000

    Update Render build script and credentials

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# Run database migrations and seed data automatically on deploy
+bundle exec rails db:migrate
+bundle exec rails db:seed
